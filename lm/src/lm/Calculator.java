c3b8e8fe5c9a6869ea2705cebfcbf4f349ad1d40
sudeep kumar yadav <sudeepnaraha@gmail.com>
2018-12-16 18:56:54 -0600

    Added calculator class

---
+++ lm/src/lm/Calculator.java
@@ -0,0 +1,14 @@
+package lm;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=6,sum;
+		sum=a+b;
+		System.out.println("sum of two numbers="+sum);
+		
+
+	}
+
+}
